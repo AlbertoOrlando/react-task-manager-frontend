3663373dd4fa290b6377aa7989d9d97771d16919
AlbertoOrlando <albertoorlandowork@google.com>
2025-06-09 20:27:16 +0200

    milestone 1 e 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import TaskList from "./pages/TaskList"
 import AddTask from "./pages/AddTask"
+import { GlobalProvider } from "./context/GlobalContext"
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
 
-      <nav>
-        <NavLink to="/">Lista delle Task</NavLink>
-        <NavLink to="/add">Aggiungi Task</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add" element={<AddTask />} />
-      </Routes>
-    </BrowserRouter>
+        <nav>
+          <NavLink to="/">Lista delle Task</NavLink>
+          <NavLink to="/add">Aggiungi Task</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/add" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
